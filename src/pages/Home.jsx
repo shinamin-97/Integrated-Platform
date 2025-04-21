435756546dd1b59c1b24e7734e1071e806febdf8
shinamin-97 <qnrlgkals@naver.com>
2025-04-22 00:36:31 +0900

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>안녕하세요. Dev_log 입니다</h1>
+            <h1>React + Vite 템플릿으로 사용되는 페이지 입니다.</h1>
+            <h1>감사합니다.</h1>
+        </div>
+    );
+}
+
+export default Home;
